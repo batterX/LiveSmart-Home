6d08b4415cd58e74d0dd464a9a16bbc4976cfc8c
batterX <ivan@visionups.com>
2022-01-11 14:52:23 +0100

    Update updater.sh
    Changed from git:// to https://

--- updater.sh
+++ updater.sh
@@ -3,7 +3,7 @@
 
 cd /home/pi
 
-git clone git://github.com/batterx/livesmart-home.git
+git clone https://github.com/batterx/livesmart-home.git
 
 sudo cp /home/pi/livesmart-home/update.sh /home/pi
 sudo chmod 777 /home/pi/update.sh
