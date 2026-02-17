3eddb0312ac7c2d6a859c86e1df3081ceb7c0e96
batterX <ivan@visionups.com>
2026-02-17 07:20:42 +0100

    v26.2.1

--- update.sh
+++ update.sh
@@ -9,6 +9,10 @@ sudo rm /home/pi/MqttStream
 sudo cp /home/pi/livesmart-home/MqttStream /home/pi
 sudo chmod 777 /home/pi/MqttStream
 
+sudo rm /home/pi/ModbusTCP
+sudo cp /home/pi/livesmart-home/ModbusTCP /home/pi
+sudo chmod 777 /home/pi/ModbusTCP
+
 sudo rm /home/pi/BMSExport
 sudo cp /home/pi/livesmart-home/BMSExport /home/pi
 sudo chmod 777 /home/pi/BMSExport
@@ -32,6 +36,7 @@ sudo apt-get install rng-tools -y
 
 sudo kill $(pgrep "BatterX")
 sudo kill $(pgrep "MqttStream")
+sudo kill $(pgrep "ModbusTCP")
 sudo kill $(pgrep "BMSExport")
 
 
